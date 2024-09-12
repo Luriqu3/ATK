1f37d5f94cfab505611f0ed3afbfb85025671a0c
Luriqu3 <mcmclhmm01@gmail.com>
2024-09-11 22:58:51 -0300

    Update ATK.lua

--- ATK.lua
+++ ATK.lua
@@ -144,3 +144,67 @@ end)
 posEnemy = addIcon("Enemy", {item = 21979, text = "Enemy"}, enemyMacro)
 posEnemy:breakAnchors()
 posEnemy:move(200, 450)
+
+
+
+
+
+    
+    function defineHotkey()
+    local tecla = tonumber(storage.tecla) or 1 
+    hotkey(tecla, "Follow", function()
+        if not g_game.isAttacking() and not g_game.isFollowing() then
+            follow(getCreatureByName(storage.seg))
+        end
+    end)
+end
+
+
+UI.Label("Tecla Do Follow")
+addTextEdit("tecla", storage.tecla or "1", function(widget, text) 
+    storage.tecla = text
+    defineHotkey()
+end)
+
+
+UI.Label("Nome")
+addTextEdit("seg", storage.seg or "nick do player", function(widget, text) 
+    storage.seg = text
+end)
+
+
+defineHotkey()
+
+
+
+macro(100, "Attack Follow Hp50%", function()
+    local target = g_game.getAttackingCreature()
+      if target and target:isPlayer() and target:getHealthPercent() <= 50 then
+          g_game.setChaseMode(1)
+    end
+end)
+
+
+
+macro(200, "ATCPK", function()
+
+for _,pla in ipairs(getSpectators(posz())) do
+
+attacked = g_game.getAttackingCreature()
+
+if not attacked or attacked:isMonster() or attacked:isPlayer() and pla:getHealthPercent() < attacked:getHealthPercent()*0.6 then
+if pla:isPlayer() and pla:getEmblem() ~= 1 and pla:getSkull() == 3 then 
+g_game.attack(pla)
+end
+end
+
+end
+
+delay(100)
+
+end)
+
+
+
+Panels.AttackLeaderTarget(batTab)  
+
